4c201502b342e5170012efdd5cae35a1a54f3ce3
Rafael Feitosa <rafael@labsit.io>
2022-02-24 17:15:56 -0300

    feat: init blank project

---
+++ src/main/kotlin/br/com/jrafael/mylife/MylifeApplication.kt
@@ -0,0 +1,11 @@
+package br.com.jrafael.mylife
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MylifeApplication
+
+fun main(args: Array<String>) {
+	runApplication<MylifeApplication>(*args)
+}
